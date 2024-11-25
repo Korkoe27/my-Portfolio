42bb2fac5adaaf32e6667a63a6752957e0bdbbb4
Mechanic <dumashieanthony@gmail.com>
2024-11-25 07:18:20 +0000

    added dark mode toggle

--- src/script.js
+++ src/script.js
@@ -11,3 +11,20 @@ function toggleHamburger() {
 
   hamburger.classList.toggle('hamburger-active');
 }
+
+
+// const body = document.body;
+// const toggleBtn = document.getElementById('toggleBtn');
+
+// if(localStorage.getItem('theme')==='dark'){
+//   body.classList.add('dark');
+//   toggleBtn.textContent = 'Light Mode';
+
+// }
+
+// toggleBtn.addEventListener('click', ()  =>{
+//   body.classList.toggle('dark');
+//   const isDark  = body.classList.contains('dark');
+//   toggleBtn.textContent = isDark  ? 'Light Mode': 'Dark Mode';
+//   localStorage.setItem('theme', isDark  ? 'dark'  : 'light');
+// })
